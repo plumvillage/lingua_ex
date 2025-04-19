57c48768ad6461255230ed2a75e0f24f0678017e
JonasCz <anandajonas9@gmail.com>
2025-04-19 17:56:53 +0200

    add checksums file

---
+++ checksum-Elixir.Lingua.Nif.exs
@@ -0,0 +1,4 @@
+%{
+  "liblingua_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:98fd47724eda671a261afd1ed9326f2a3042fc7a42fe3cbbfcf3e6a106865a0b",
+  "liblingua_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:b5e7d29d17d012975ffb581417f55562b15906730bdfc099793ce585331107c5"
+}
