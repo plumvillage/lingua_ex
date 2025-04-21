51597d0cdf42c978dfd59794c23dde17fa68de12
JonasCz <anandajonas9@gmail.com>
2025-04-21 12:09:41 +0200

    add missing checksum for linux arm64

--- checksum-Elixir.Lingua.Nif.exs
+++ checksum-Elixir.Lingua.Nif.exs
@@ -1,4 +1,5 @@
 %{
   "liblingua_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:98fd47724eda671a261afd1ed9326f2a3042fc7a42fe3cbbfcf3e6a106865a0b",
-  "liblingua_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:b5e7d29d17d012975ffb581417f55562b15906730bdfc099793ce585331107c5"
+  "liblingua_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:b5e7d29d17d012975ffb581417f55562b15906730bdfc099793ce585331107c5",
+  "liblingua_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:be8d58bce8a85241ddf8d200d985f5d29f582546f1ebac7dac0ec0502131bd70"
 }
